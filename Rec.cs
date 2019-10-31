40f5335209683ca82b017afde703073357ed817d
vuong1234 <55830628+vuong1234@users.noreply.github.com>
2019-10-31 14:34:29 +0700

    Add files via upload

---
+++ Rec.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    public class Rec
+    {
+        private float Lenght = 1;
+        private float Width = 1;
+
+        public Rec()
+        {
+
+        }
+
+        public Rec(float Lenght, float Width)
+        {
+            this.Lenght = Lenght;
+            this.Width = Width;
+        }
+
+        public float  GetLenght()
+        {
+            return Lenght;
+        }
+
+        public void SetLenght(float NewLenght)
+        {
+            Lenght = NewLenght;
+        }
+
+        public float GetWidth()
+        {
+            return Width;
+        }
+
+        public void SetWidth(float NewWidth)
+        {
+            Width = NewWidth;
+        }
+
+        public double GetArea()
+        {
+            return this.Width * this.Lenght;
+        }
+
+        public double GetPerimeter()
+        {
+            return (this.Lenght + this.Width) * 2;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
