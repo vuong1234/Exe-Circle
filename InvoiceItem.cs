6c7ce99db8153d62314ee97468220ef8cb290027
vuong1234 <55830628+vuong1234@users.noreply.github.com>
2019-10-31 14:36:01 +0700

    Add files via upload

---
+++ InvoiceItem.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceItem
+{
+    public class InvoiceItem
+    {
+        private string Id;
+        private string Desc;
+        private int Qty;
+        private double UnitPrice;
+
+        public InvoiceItem( string ID, string De, int Qt, double Un)
+        {
+            Id = ID;
+            Desc = De;
+            Qty = Qt;
+            UnitPrice = Un;
+        }
+
+        public string GetId()
+        {
+            return Id;
+        }
+
+        public string GetDesc()
+        {
+            return Desc;
+        }
+
+        public int GetQty()
+        {
+            return Qty;
+        }
+
+        public void SetQty( int NewQty)
+        {
+            Qty = NewQty;
+        }
+
+        public double GetUnitPrice()
+        {
+            return UnitPrice;
+        }
+
+        public void SetUnitPrice( double NewUnitPrice)
+        {
+            UnitPrice = NewUnitPrice;
+        }
+
+        public double GetTotal()
+        {
+            return 1;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
